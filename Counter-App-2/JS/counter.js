bc50e7df3eac6ccd6a9567e2cd1f42134131ce3f
bgroyer1 <royerbraedan@gmail.com>
2023-12-02 18:06:19 -0800

    Uploaded Files
    First draft of my passenger counter app

---
+++ Counter-App-2/JS/counter.js
@@ -0,0 +1,14 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let incrementBtn = document.getElementById("increment-btn")
+let saveBtn = document.querySelector("#save-btn")
+let count = 0;
+
+incrementBtn.addEventListener('click', () => {
+    count += 1;
+    countEl.textContent = `People Entered: ${count}`
+})
+
+saveBtn.addEventListener('click', () => {
+    saveEl.textContent += ` ${count} - `
+})
